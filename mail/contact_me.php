8b21df630a10491e0bbae3927e7e355ce6e760f0
SRowdyTG <rodrigomontes.biz@gmail.com>
2020-04-01 18:07:37 -0300

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) ||  empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
 }
@@ -13,8 +13,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "rodrigomontes.biz@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
+$header = "From: noreply@srowdytg.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body))
+if(!mail($to, $subject, $body, $header))
   http_response_code(500);
 ?>
